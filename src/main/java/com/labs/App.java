b110dff6b6b9b11fde5b806f13ca2120665cee9c
Yuntaichen <yuntaichen1@gmail.com>
2021-05-29 20:23:18 +0300

    Add CRUD methods

--- src/main/java/com/labs/App.java
+++ src/main/java/com/labs/App.java
@@ -4,7 +4,7 @@ import javax.xml.ws.Endpoint;
 
 public class App {
     public static void main(String[] args) {
-        String url = "http://0.0.0.0:8080/FindStudentsService";
+        String url = "http://0.0.0.0:8080/CRUDService";
         Endpoint.publish(url, new StudentWebService());
     }
 }
--- src/main/java/com/labs/PostgreSQLDAO.java
+++ src/main/java/com/labs/PostgreSQLDAO.java
@@ -5,6 +5,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.logging.Logger;
 import java.util.logging.Level;
 
@@ -13,8 +14,8 @@ public class PostgreSQLDAO {
         LinkedHashSet<Student> students = new LinkedHashSet<Student>();
         try (Connection connection = ConnectionUtil.getConnection()) {
             Statement stmt = connection.createStatement();
-            for (int i = 0; i < searchArgs.length; i++) {
-                ResultSet rs = stmt.executeQuery("SELECT t.* FROM students t WHERE (t.*)::text LIKE '%" + searchArgs[i] + "%'");
+            for (String searchArg : searchArgs) {
+                ResultSet rs = stmt.executeQuery("SELECT t.* FROM students t WHERE (t.*)::text LIKE '%" + searchArg + "%'");
                 while (rs.next()) {
                     String name = rs.getString("name");
                     String surname = rs.getString("surname");
@@ -31,4 +32,53 @@ public class PostgreSQLDAO {
         }
         return students;
     }
+
+    public String createStudent(String name, String surname, int age, int studentId, String mark) {
+        String status = "0";
+
+        try (Connection connection = ConnectionUtil.getConnection()) {
+            Statement stmt = connection.createStatement();
+            System.out.println("INSERT INTO students(name, surname, age, student_id, mark) values ('" +
+                    name + "', '" + surname + "', " + age + ", " + studentId + ", '" + mark + "');");
+            int rs = stmt.executeUpdate("INSERT INTO students(name, surname, age, student_id, mark) values ('" +
+                    name + "', '" + surname + "', " + age + ", " + studentId + ", '" + mark + "');");
+            status = Integer.toString(rs);
+
+        } catch (SQLException ex) {
+            Logger.getLogger(PostgreSQLDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return status;
+    }
+
+    public String deleteStudent(int rowId) {
+        String status = "0";
+
+        try (Connection connection = ConnectionUtil.getConnection()) {
+            Statement stmt = connection.createStatement();
+            int rs = stmt.executeUpdate("DELETE FROM students WHERE id='" + rowId + "';");
+            status = Integer.toString(rs);
+        } catch (SQLException ex) {
+            Logger.getLogger(PostgreSQLDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return status;
+
+    }
+
+    public String updateStudent(int rowId, List<String> updateArgs) {
+        String status = "0";
+        String updateFields = String.join(", ", updateArgs);
+
+        try (Connection connection = ConnectionUtil.getConnection()) {
+            Statement stmt = connection.createStatement();
+            int rs = stmt.executeUpdate("UPDATE students SET " + updateFields + " WHERE id=" + rowId + ";");
+            status = Integer.toString(rs);
+        } catch (SQLException ex) {
+            Logger.getLogger(PostgreSQLDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return status;
+    }
+
 }
